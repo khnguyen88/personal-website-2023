08a42e235e1e40668d30fbefe6116630cfe9738e
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-03-20 23:19:23 -0400

    Created and populated navigation bar with links to webpages and associated templates.

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  menuItems: any[];
+  menuItemHome: any;
+
+  ngOnInit(){
+    this.menuItems = [
+      {label: 'Home', route: 'home'},
+      {label: 'Resume', route: 'resume'},
+      {label: 'Interests', route: 'interests'},
+      {label: 'Projects', route: 'projects'},
+      {label: 'Gallery', route: 'gallery'},
+      {label: 'Contact', route: 'contact'},
+    ];
+
+    this.menuItemHome = {label: 'Home', route: 'home'};
+
+  }
 
 }
