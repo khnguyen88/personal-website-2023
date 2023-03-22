7f7395922b6800903ff45dea3c3ef6f595dbcbf4
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-03-21 21:08:31 -0400

    Imported and utilized the HttpClient module to utilize the http Get request to retrieve the JSON data from the local source.

--- src/app/service/resume.service.ts
+++ src/app/service/resume.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResumeService {
 
-  constructor() { }
+  constructor(private httpService: HttpClient) { }
+
+  getResume(){
+    return this.httpService.get('../assets/json/resume.json');
+  }
 }
