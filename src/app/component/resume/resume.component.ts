5ee5ccbbdbbc34902afcebf63bc7c47bb47199ae
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-04-11 01:17:59 -0400

    Updated resume component to extend the base component and implement OnInit lifestyle hook. Updated ngOnInit() method to utilized pipe to link the takeUntil operator to manage subscription

--- src/app/component/resume/resume.component.ts
+++ src/app/component/resume/resume.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { takeUntil } from 'rxjs/operators';
 import { ResumeService } from 'src/app/service/resume.service';
+import { BaseComponent } from '../base-component/base-component.component';
+
 
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.scss']
 })
-export class ResumeComponent {
+export class ResumeComponent extends BaseComponent implements OnInit {
   resume: any;
 
-  constructor(private resumeService: ResumeService){}
+  constructor(private resumeService: ResumeService) {
+    super()
+  }
 
   ngOnInit() {
-    this.resumeService.getResume().subscribe(result => this.resume = result);
+    this.resumeService.getResume().pipe(takeUntil(this.componentDestroyed$)).subscribe(result => this.resume = result);
   }
 
 }
