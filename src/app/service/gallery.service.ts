5b940d558df61cbf62292506fd6dbee1224eb8d5
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-04-16 14:54:12 -0400

    Corrected minor typo in comment for gallery.service.ts

--- src/app/service/gallery.service.ts
+++ src/app/service/gallery.service.ts
@@ -9,8 +9,7 @@ export class GalleryService {
   // Imported and injected the HttpClient service through the class constructor's parameter
   constructor(private httpService: HttpClient) { }
 
-  // The getResume() method retrieves and return gallery images data from JSON
-
+  // The getGalleryImages() method retrieves and return gallery images data from JSON
   getGalleryImages() {
 
     // This method constructs and calls the HttpClient's get() method, which creates an observable and calls a get request to
