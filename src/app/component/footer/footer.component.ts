89efde9702172452a6320b378700647669cd1d6e
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-04-17 00:51:38 -0400

    Populated footer component and template. Imported awesome icon components and icons

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faCoffee, faSquare, faCheckSquare} from '@fortawesome/free-solid-svg-icons'; // Imported icons to test
+import { faGithub, faLinkedin, faInstagram, faStackOverflow, faYoutube} from '@fortawesome/free-brands-svg-icons'; // Imported icons to use
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  constructor(library: FaIconLibrary) {
+    library.addIcons(
+      faCoffee,
+      faSquare,
+      faCheckSquare,
+      faLinkedin,
+      faInstagram,
+      faStackOverflow,
+      faGithub,
+      faYoutube
+    );
+  }
 }
