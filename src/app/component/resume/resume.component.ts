1ef157967f2f9bb673cb90ece94fda75dcd1a806
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-04-17 22:25:16 -0400

    Updated resume component comments

--- src/app/component/resume/resume.component.ts
+++ src/app/component/resume/resume.component.ts
@@ -10,14 +10,14 @@ import { BaseComponent } from '../base-component/base-component.component';
   styleUrls: ['./resume.component.scss']
 })
   
-// We extended the GalleryComponent with the BaseComponent because we need to subscribe to an observable to retrieve our data, and the
+// We extended the ResumeComponent with the BaseComponent because we need to subscribe to an observable to retrieve our data, and the
 // BaseComponent ensure we unsubscribe to the observable once component is destroyed  
   
 // We implemented the OnInit interface and used it to initialize the data used to populate the template
 export class ResumeComponent extends BaseComponent implements OnInit {
 
   // The resume property holds the JSON data retrieve as an array of objects
-  // To be used to populate data in the Gallery Template and we bpage
+  // To be used to populate data in the Resume Template and we bpage
   resume: any;
 
   // Imported and injected the Resume Service through the class constructor's parameter
