1cf0aabce63f5c1ed98030cea5952c79e49257cf
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-04-19 22:19:59 -0400

    Updated the gallery component to utilize local storage to save and retrieve the collection of GalleryData from there and assign it to the property.
    Added a button to reset the gallery to the original ordered state. Added comments to both gallery template and componnet.
    
    Adjusted the styles.

--- src/app/component/gallery/gallery.component.ts
+++ src/app/component/gallery/gallery.component.ts
@@ -20,6 +20,16 @@ export class GalleryComponent extends BaseComponent implements OnInit {
   // To be used to populate data in the Gallery Template and image elements attributes
   imageGallery: any;
 
+
+  // Check if shuffledGalleryState has been stored in a local storage (Web Storage). 
+  // If not use localStorage to save file. This is my alternative to cookies for now. I need to figure out to get Angular to talk to a server-side framework to set and retrieve cookies.
+  // Downside with localStorage is it does not really expire and only gets removed when browser cache is cleared.
+  // source: https://stackoverflow.com/questions/29960037/localstorage-vs-sessionstorage-vs-cookies
+  isGalleryShuffled: boolean = localStorage.getItem('shuffledCollection') ? true : false;
+
+  // Check if normallyGalleryState has been stored in a local storage
+  isOriginalGalleryLocal: boolean = localStorage.getItem('imageCollection') ? true : false;
+
   // Stores the base directory of asset images folder
   // To be used to populate data in the Gallery Template and image elements attributes
   baseImageAssetDirectory: string = "../../../assets/images/";
@@ -34,13 +44,23 @@ export class GalleryComponent extends BaseComponent implements OnInit {
   // ngOnInit is a callback method that gets called after the Component is initialized
   ngOnInit() {
 
-    // We call and subscribe to the getGalleryImage() method to retrieve gallery images data from JSON file and store it within a property
-    // We utilized takeUntil to declarative unsubscribe to observable after Component is destroyed
-    this.galleryService.getGalleryImages().pipe(takeUntil(this.componentDestroyed$)).subscribe(result => {
-      this.imageGallery = result;
-      this.shuffleArray(this.imageGallery); // We shuffle the image gallery initially
-      this.cdr.detectChanges(); //Since we updated changeDetection to OnPush, we need to call the detectChanges() method here.
-    });
+    // We check if the shuffled imageGallery data has been saved locally via Web Storage
+    // If not, we'll just retrieve the normal imageGallery data from JSON and set it.
+    if(!this.isGalleryShuffled){
+      // We call and subscribe to the getGalleryImage() method to retrieve gallery images data from JSON file and store it within a property
+      // We utilized takeUntil to declarative unsubscribe to observable after Component is destroyed
+      this.galleryService.getGalleryImages().pipe(takeUntil(this.componentDestroyed$)).subscribe(result => {
+        this.imageGallery = result;
+        localStorage.setItem('imageCollection', JSON.stringify(this.imageGallery));
+        localStorage.setItem('shuffledCollection', JSON.stringify(this.imageGallery));
+        this.cdr.detectChanges(); //Since we updated changeDetection to OnPush, we need to call the detectChanges() method here.
+      });
+    }
+    else {
+      // We assign the property imageGallery to the data we retrieve from shuffled Gallery collection localStorage, if it does not exist, we to the orginal collection or nothing.
+      this.imageGallery = JSON.parse(localStorage.getItem('shuffledCollection') ||
+        localStorage.getItem('imageCollection') || '{}');
+    }
   }
 
   // shuffleArray() method - shuffle order of an array. To be used to shuffle images in gallery
@@ -59,6 +79,25 @@ export class GalleryComponent extends BaseComponent implements OnInit {
   // Event handling method used to bind to click event. Intended to shuffle gallery on click.
   onClickShuffle() {
     this.shuffleArray(this.imageGallery); // We changed the data of the page content here.
+    localStorage.setItem('shuffledCollection', JSON.stringify(this.imageGallery)); // We store the new shuffled gallery data in the local storage
     this.cdr.detectChanges(); //Since we updated changeDetection to OnPush, we need to call the detectChanges() method here.
+  }
+
+    // Event handling method used to bind to click event. Intended to reset gallery on click.
+  onClickReset() {
+    // We check if the original imageGallery data has been saved locally via Web Storage. If so we assigned it to the imageGallery property
+    if (this.isOriginalGalleryLocal) {
+            this.imageGallery = JSON.parse(localStorage.getItem('imageCollection') || '{}');
+    }
+    else {
+      // Else, We recall and subscribe to the getGalleryImage() method to retrieve gallery images data from JSON file and store it within a property
+      // We utilized takeUntil to declarative unsubscribe to observable after Component is destroyed
+      this.galleryService.getGalleryImages().pipe(takeUntil(this.componentDestroyed$)).subscribe(result => {
+        this.imageGallery = result; // We assigned the imageryGallery property to the result.
+        localStorage.setItem('imageCollection', JSON.stringify(this.imageGallery)); // We set the imageCollection local storage
+        localStorage.setItem('shuffledCollection', JSON.stringify(this.imageGallery)); // We reset the shuffleCollection local storage
+        this.cdr.detectChanges(); //Since we updated changeDetection to OnPush, we need to call the detectChanges() method here.
+      });
+    }
   }
 }
